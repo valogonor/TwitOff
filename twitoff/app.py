2d0313385a2049eb27072167044fc6d2508658cf
Valerie Ogonor <vogonor1@students.kennesaw.edu>
2019-03-05 23:50:31 -0500

    Adding module2 updates

---
+++ twitoff/app.py
@@ -0,0 +1,26 @@
+'''Main application and routing logic for TwitOff.'''
+from decouple import config
+from flask import Flask, render_template, request
+from .models import DB, User
+
+
+def create_app():
+    '''Create and configure an instance of the Flask application.'''
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['ENV'] = config('ENV')
+    DB.init_app(app)
+
+    @app.route('/')
+    def root():
+        users = User.query.all()
+        return render_template('base.html', title='Home', users=users)
+
+    @app.route('/reset')
+    def reset():
+        DB.drop_all()
+        DB.create_all()
+        return render_template('base.html', title='DB Reset!', users=[])
+
+    return app
